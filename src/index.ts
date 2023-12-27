532b9e9cc36a95fce5f37e67be87a41bf6aa816b
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-27 13:59:14 -0500

    use postcss-load-config package

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,35 @@ import postcss, { AcceptedPlugin } from "postcss";
 import postcssrc, { type Config } from "postcss-load-config"
 import { type BunPlugin } from "bun";
 
+const processCss = async (from: string, to: string, plugins: any) => {
+    const inputFile = Bun.file(isAbsolute(from) ? from : join(process.cwd(), from));
+    const outputFile = Bun.file(isAbsolute(to) ? to : join(process.cwd(), to));
+    const result = await postcss(plugins as AcceptedPlugin[] ?? []).process(await inputFile.text(), { from: inputFile.name, to: outputFile.name })
+    await Bun.write(outputFile, result.css);
+    console.log('[bun-postcss]', `Compiled ${from} to ${to}`);
+    return
+}
+
 export const bunPostcss = (config: Config | null = null): BunPlugin => ({
     name: "bun-postcss",
     async setup() {
         try {
-            if (!config) config = await postcssrc();
-            if (!config) throw new Error('no postcss config found')
-            if (!config.to || !config.from) throw new Error('"to" and "from" must be specified')
-
-            let inputFile = Bun.file(isAbsolute(config.from) ? config.from : join(process.cwd(), config.from));
-            let outputFile = Bun.file(isAbsolute(config.to) ? config.to : join(process.cwd(), config.to));
-            const result = await postcss(config.plugins as AcceptedPlugin[] ?? []).process(await inputFile.text(), { from: inputFile.name, to: outputFile.name })
-            await Bun.write(outputFile, result.css);
-            console.log('[bun-postcss]', `Compiled ${config.from} to ${config.to}`);
+            let from, to, plugins;
+            if (config) {
+                from = config.from;
+                to = config.to;
+                plugins = config.plugins;
+            } else {
+                const loaded = await postcssrc();
+                if (!loaded) throw new Error('no postcss config found')
+                from = loaded.options.from;
+                to = loaded.options.to;
+                plugins = loaded.plugins;
+            }
+            if (from && !to) await processCss(from, from.replace(/\.css$/, '_out.css'), plugins)
+            else if (from && to) await processCss(from, to, plugins)
         } catch (e) {
             console.error('[bun-postcss]', `Failed to compile:`, e);
         }
     }
-});
-
+})
